936bfebdc6254e7bcd99f35e56bf84ebefb0ae28
Mai Vang <vmaineng@gmail.com>
2024-02-05 18:10:00 -0800

    added in extra files

---
+++ .venv/fundamentals.py
@@ -0,0 +1,50 @@
+print("Hello world!") #string = sequence of characters
+
+#print = function built into python and print a message onto the application window
+
+age = 20
+print(age)
+
+#instead of camelCase use first_name
+
+#False = boolean false value
+
+#input = built in function read values from the window
+name = input("What is your name? ")
+print("Hello " + name) #concentation
+
+#string, number, and boolean = data type
+
+#typeError: int vs str
+
+birth_year = input("Enter your birth year: ")
+age = 2020 - int(birth_year) #have to wrap int around birth_year b/c it's a string
+print(age)
+
+#built in function:
+#float() = convert value to a floating point number
+#bool() = convert value to boolean
+#str()
+#int()
+
+#calculator:
+first = input("type in first number ")
+second = input("type in second number ")
+sum = int(first) + int(second)
+print (sum)
+
+#methods = functions tied to the object
+course = 'PYthon for Beginners' #strings are 0 index
+print(course.find('y'))
+#strings are immutable
+
+#print( python in course) => true
+
+
+
+
+
+
+
+
+
